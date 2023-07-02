a431c6eca98f28a1a28aa99d4f3a86c9481119cb
AndresCortesA <andrescortes.22311@gmail.com>
2023-07-02 04:29:16 -0500

    "Conversor con plus de temperatura, version 1 con java swing"

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,113 +1,131 @@
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 public class CurrencyConverter {
     public static void main(String[] args) {
         // Menú principal
-        String[] options = { "COP a Extranjera", "COP a Extranjera Detalles", "Extranjera a COP", "Temperatura" };
+        String[] options = { "COP a Extranjera", "COP a Extranjera Detalles", "Extranjera a COP", "Temperatura", "Salir" };
         int selectedOption = JOptionPane.showOptionDialog(null, "Seleccione una opción:", "Conversor de Divisas",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
 
-        if (selectedOption == 0) {
-            // Menú para convertir entre COP y moneda extranjera
-            String[] conversionOptions = { "USD", "EUR", "GBP", "JPY", "KRW" };
-            String selectedCurrency = (String) JOptionPane.showInputDialog(null,
-                    "Seleccione la moneda extranjera a convertir:", "Conversor de Divisas", JOptionPane.PLAIN_MESSAGE,
-                    null, conversionOptions, conversionOptions[0]);
-
-            // Solicitar cantidad de dinero en COP al usuario
-            String amountInput = JOptionPane.showInputDialog(null, "Ingrese la cantidad de dinero (COP):");
-            double amount = Double.parseDouble(amountInput);
-
-            // Realizar la conversión seleccionada
-            double convertedAmount = switch (selectedCurrency) {
-                case "USD" -> convertToUSD(amount);
-                case "EUR" -> convertToEUR(amount);
-                case "GBP" -> convertToGBP(amount);
-                case "JPY" -> convertToJPY(amount);
-                default -> convertToKRW(amount);
-            };
-
-            // Mostrar resultado en un cuadro de diálogo
-            JOptionPane.showMessageDialog(null,
-                    "Cantidad a convertir (COP): " + amount + "\n" +
-                            "Cantidad convertida (" + selectedCurrency + "): " + convertedAmount);
-        } else if (selectedOption == 1) {
-            // Menú para convertir de COP a todas las monedas extranjeras
-            // Solicitar cantidad de dinero en COP al usuario
-            String amountInput = JOptionPane.showInputDialog(null, "Ingrese la cantidad de dinero (COP):");
-            double amount = Double.parseDouble(amountInput);
-
-            // Realizar las conversiones a todas las monedas extranjeras
-            double usd = convertToUSD(amount);
-            double eur = convertToEUR(amount);
-            double gbp = convertToGBP(amount);
-            double jpy = convertToJPY(amount);
-            double krw = convertToKRW(amount);
-
-            // Mostrar resultados en un cuadro de diálogo
-            JOptionPane.showMessageDialog(null,
-                    "Cantidad a convertir (COP): " + amount + "\n\n" +
-                            "Conversiones a otras monedas:\n" +
-                            "Dólares (USD): " + usd + "\n" +
-                            "Euros (EUR): " + eur + "\n" +
-                            "Libras Esterlinas (GBP): " + gbp + "\n" +
-                            "Yen Japonés (JPY): " + jpy + "\n" +
-                            "Won surcoreano (KRW): " + krw);
-        } else if (selectedOption == 2) {
-            // Menú para convertir de todas las monedas extranjeras a COP
-            String[] conversionOptions = { "USD", "EUR", "GBP", "JPY", "KRW" };
-            String selectedCurrency = (String) JOptionPane.showInputDialog(null,
-                    "Seleccione la moneda extranjera a convertir a COP:", "Conversor de Divisas",
-                    JOptionPane.PLAIN_MESSAGE, null, conversionOptions, conversionOptions[0]);
-
-            // Solicitar cantidad de dinero en la moneda extranjera al usuario
-            String amountInput = JOptionPane.showInputDialog(null,
-                    "Ingrese la cantidad de dinero en " + selectedCurrency + ":");
-            double amount = Double.parseDouble(amountInput);
-
-            // Realizar la conversión a COP
-            double convertedAmount = switch (selectedCurrency) {
-                case "USD" -> convertUSDtoCOP(amount);
-                case "EUR" -> convertEURtoCOP(amount);
-                case "GBP" -> convertGBPtoCOP(amount);
-                case "JPY" -> convertJPYtoCOP(amount);
-                default -> convertKRWtoCOP(amount);
-            };
-
-            // Mostrar resultado en un cuadro de diálogo
-            JOptionPane.showMessageDialog(null,
-                    "Cantidad a convertir (" + selectedCurrency + "): " + amount + "\n" +
-                            "Cantidad convertida (COP): " + convertedAmount);
-        } else if (selectedOption == 3) {
-            // Conversión de temperatura
-            String[] temperatureOptions = { "Celsius a Fahrenheit", "Fahrenheit a Celsius" };
-            int selectedTemperatureOption = JOptionPane.showOptionDialog(null, "Seleccione la conversión de temperatura:",
-                    "Conversor de Temperatura", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null,
-                    temperatureOptions, temperatureOptions[0]);
-
-            if (selectedTemperatureOption == 0) {
-                // Conversión de Celsius a Fahrenheit
-                String temperatureInput = JOptionPane.showInputDialog(null, "Ingrese la temperatura en Celsius:");
-                double celsius = Double.parseDouble(temperatureInput);
-                double fahrenheit = convertCelsiusToFahrenheit(celsius);
-
-                JOptionPane.showMessageDialog(null,
-                        "Temperatura en Celsius: " + celsius + "\n" +
-                                "Temperatura en Fahrenheit: " + fahrenheit);
-            } else if (selectedTemperatureOption == 1) {
-                // Conversión de Fahrenheit a Celsius
-                String temperatureInput = JOptionPane.showInputDialog(null, "Ingrese la temperatura en Fahrenheit:");
-                double fahrenheit = Double.parseDouble(temperatureInput);
-                double celsius = convertFahrenheitToCelsius(fahrenheit);
-
-                JOptionPane.showMessageDialog(null,
-                        "Temperatura en Fahrenheit: " + fahrenheit + "\n" +
-                                "Temperatura en Celsius: " + celsius);
+        while (selectedOption != 4) {
+            switch (selectedOption) {
+                case 0 -> convertCOPtoForeign();
+                case 1 -> convertCOPtoForeignDetails();
+                case 2 -> convertForeignToCOP();
+                case 3 -> convertTemperature();
             }
+
+            selectedOption = JOptionPane.showOptionDialog(null, "Seleccione una opción:", "Conversor de Divisas",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
         }
 
-        // Cerrar el programa
+        // Salir del programa
         System.exit(0);
+    }
+
+    private static void convertCOPtoForeign() {
+        // Menú para convertir entre COP y moneda extranjera
+        String[] conversionOptions = { "USD", "EUR", "GBP", "JPY", "KRW" };
+        String selectedCurrency = (String) JOptionPane.showInputDialog(null,
+                "Seleccione la moneda extranjera a convertir:", "Conversor de Divisas", JOptionPane.PLAIN_MESSAGE,
+                null, conversionOptions, conversionOptions[0]);
+
+        // Solicitar cantidad de dinero en COP al usuario
+        String amountInput = JOptionPane.showInputDialog(null, "Ingrese la cantidad de dinero (COP):");
+        double amount = Double.parseDouble(amountInput);
+
+        // Realizar la conversión seleccionada
+        double convertedAmount = switch (selectedCurrency) {
+            case "USD" -> convertToUSD(amount);
+            case "EUR" -> convertToEUR(amount);
+            case "GBP" -> convertToGBP(amount);
+            case "JPY" -> convertToJPY(amount);
+            default -> convertToKRW(amount);
+        };
+
+        // Mostrar resultado en un cuadro de diálogo
+        JOptionPane.showMessageDialog(null,
+                "Cantidad a convertir (COP): " + amount + "\n" +
+                        "Cantidad convertida (" + selectedCurrency + "): " + convertedAmount);
+    }
+
+    private static void convertCOPtoForeignDetails() {
+        // Menú para convertir de COP a todas las monedas extranjeras
+        // Solicitar cantidad de dinero en COP al usuario
+        String amountInput = JOptionPane.showInputDialog(null, "Ingrese la cantidad de dinero (COP):");
+        double amount = Double.parseDouble(amountInput);
+
+        // Realizar las conversiones a todas las monedas extranjeras
+        double usd = convertToUSD(amount);
+        double eur = convertToEUR(amount);
+        double gbp = convertToGBP(amount);
+        double jpy = convertToJPY(amount);
+        double krw = convertToKRW(amount);
+
+        // Mostrar resultados en un cuadro de diálogo
+        JOptionPane.showMessageDialog(null,
+                "Cantidad a convertir (COP): " + amount + "\n\n" +
+                        "Conversiones a otras monedas:\n" +
+                        "Dólares (USD): " + usd + "\n" +
+                        "Euros (EUR): " + eur + "\n" +
+                        "Libras Esterlinas (GBP): " + gbp + "\n" +
+                        "Yen Japonés (JPY): " + jpy + "\n" +
+                        "Won surcoreano (KRW): " + krw);
+    }
+
+    private static void convertForeignToCOP() {
+        // Menú para convertir de todas las monedas extranjeras a COP
+        String[] conversionOptions = { "USD", "EUR", "GBP", "JPY", "KRW" };
+        String selectedCurrency = (String) JOptionPane.showInputDialog(null,
+                "Seleccione la moneda extranjera a convertir a COP:", "Conversor de Divisas",
+                JOptionPane.PLAIN_MESSAGE, null, conversionOptions, conversionOptions[0]);
+
+        // Solicitar cantidad de dinero en la moneda extranjera al usuario
+        String amountInput = JOptionPane.showInputDialog(null,
+                "Ingrese la cantidad de dinero en " + selectedCurrency + ":");
+        double amount = Double.parseDouble(amountInput);
+
+        // Realizar la conversión a COP
+        double convertedAmount = switch (selectedCurrency) {
+            case "USD" -> convertUSDtoCOP(amount);
+            case "EUR" -> convertEURtoCOP(amount);
+            case "GBP" -> convertGBPtoCOP(amount);
+            case "JPY" -> convertJPYtoCOP(amount);
+            default -> convertKRWtoCOP(amount);
+        };
+
+        // Mostrar resultado en un cuadro de diálogo
+        JOptionPane.showMessageDialog(null,
+                "Cantidad a convertir (" + selectedCurrency + "): " + amount + "\n" +
+                        "Cantidad convertida (COP): " + convertedAmount);
+    }
+
+    private static void convertTemperature() {
+        // Conversión de temperatura
+        String[] temperatureOptions = { "Celsius a Fahrenheit", "Fahrenheit a Celsius" };
+        int selectedTemperatureOption = JOptionPane.showOptionDialog(null, "Seleccione la conversión de temperatura:",
+                "Conversor de Temperatura", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null,
+                temperatureOptions, temperatureOptions[0]);
+
+        if (selectedTemperatureOption == 0) {
+            // Conversión de Celsius a Fahrenheit
+            String temperatureInput = JOptionPane.showInputDialog(null, "Ingrese la temperatura en Celsius:");
+            double celsius = Double.parseDouble(temperatureInput);
+            double fahrenheit = convertCelsiusToFahrenheit(celsius);
+
+            JOptionPane.showMessageDialog(null,
+                    "Temperatura en Celsius: " + celsius + "\n" +
+                            "Temperatura en Fahrenheit: " + fahrenheit);
+        } else if (selectedTemperatureOption == 1) {
+            // Conversión de Fahrenheit a Celsius
+            String temperatureInput = JOptionPane.showInputDialog(null, "Ingrese la temperatura en Fahrenheit:");
+            double fahrenheit = Double.parseDouble(temperatureInput);
+            double celsius = convertFahrenheitToCelsius(fahrenheit);
+
+            JOptionPane.showMessageDialog(null,
+                    "Temperatura en Fahrenheit: " + fahrenheit + "\n" +
+                            "Temperatura en Celsius: " + celsius);
+        }
     }
 
     // Conversión a Dólares (USD)
@@ -177,7 +195,7 @@ public class CurrencyConverter {
         return foreignAmount / conversionRate;
     }
 
-// Conversión de Celsius a Fahrenheit (°C a °F)
+    // Conversión de Celsius a Fahrenheit (°C a °F)
     private static double convertCelsiusToFahrenheit(double celsius) {
         return (celsius * 9.0 / 5.0) + 32.0;
     }
